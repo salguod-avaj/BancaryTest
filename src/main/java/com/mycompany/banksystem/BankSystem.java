ad4e955884a4187ab02f77b31b17043e863faf79
douglassousa-hash <douglas.sousa@ucsal.edu.br>
2025-09-10 20:54:53 -0300

    new Menu System

--- src/main/java/com/mycompany/banksystem/BankSystem.java
+++ src/main/java/com/mycompany/banksystem/BankSystem.java
@@ -53,23 +53,10 @@ public class BankSystem {
                 System.out.println(x.companyRelatory());
             }
             
+            optionsMenu(list, sc);
             
             
-            System.out.println("After one month"); // test
-            for(Account x : list) {
-                if(x instanceof CurrentAccount) {
-                    CurrentAccount z = (CurrentAccount) x;
-                    z.mensalTax();
-                    x.deposit(600);
-                    x.withdraw(500);
-                }
-            }
             
-            for(Account x : list) {
-                System.out.println("\n" + x.companyRelatory());
-            }
-            
-            sc.close();
         }
         catch(InputMismatchException e) {
             System.out.println("Invalid Type");
@@ -83,6 +70,84 @@ public class BankSystem {
         catch(InvalidBalanceException e) {
             System.out.println(e.getMessage());
         }
+        finally{
+            sc.close();
+        }
         
+    }
+    
+    public static void optionsMenu(List<Account> list, Scanner sc) {
+        int newChoice;
+        do {
+           
+                System.out.println("[1] withdraw");
+                System.out.println("[2] deposit");
+                System.out.println("[3] create a new account");
+                System.out.println("[4] delete a created account");
+                System.out.println("[5] show relatory");
+                System.out.println("[6] quit");
+                
+                newChoice = sc.nextInt();
+                switch(newChoice) {
+                    case 1:
+                        System.out.println("Insert the id");
+                        int newId = sc.nextInt();
+                        for(Account x : list) {
+                            if(newId == x.getId()) {
+                                System.out.println("Enter the amount to withdraw: ");
+                                double withdraw = sc.nextDouble();
+                                x.withdraw(withdraw);
+                            }
+                        }
+                        break;
+                    case 2:
+                        System.out.println("Insert the id: ");
+                        newId = sc.nextInt();
+                        for(Account x : list) {
+                            if(newId == x.getId()) {
+                                System.out.println("Enter the amount to deposit: ");
+                                double deposit = sc.nextDouble();
+                                x.deposit(deposit);
+                            }
+                        }
+                        break;
+                    case 3:
+                        sc.nextLine();
+                        System.out.println("Enter your name: ");
+                        String name = sc.nextLine();
+                        System.out.println("Enter a id: ");
+                        newId = sc.nextInt();
+                        System.out.println("Enter a initial deposit: ");
+                        Double newInitialDeposit = sc.nextDouble();
+                        System.out.println("Enter your account type [C/S]");
+                        char typeChoice = sc.next().toLowerCase().charAt(0);
+                        if(typeChoice == 'c') {
+                            list.add(new CurrentAccount(5,0.01, name, newId, newInitialDeposit));
+                        }
+                        else if(typeChoice == 's') {
+                            list.add(new SavingsAccount(0.005, name, newId, newInitialDeposit));
+                        }
+                        break;
+                    case 4:
+                        System.out.println("Enter the id of the account to delete: ");
+                        int idToDelete = sc.nextInt();
+                        boolean removed = list.removeIf(acc -> acc.getId() == idToDelete);
+                        if(removed) {
+                            System.out.println("Account successfully removed;");
+                        }
+                        else {
+                            System.out.println("This id does not exist");
+                        }
+                        break;
+                    case 5:
+                        for(Account x : list) {
+                            System.out.println(x.companyRelatory());
+                        }
+                        break;
+                    case 6:
+                        System.out.println("System has quit");
+                        
+                }
+            } while(newChoice != 6);  
     }
 }
